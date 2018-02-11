2426d562daf2365ca43c1c9a769b385d6517746e
Neelam1996 <36359791+Neelam1996@users.noreply.github.com>
2018-02-12 02:05:16 +0530

    Add files via upload

---
+++ Longest_Substring_Without_Repeat.java
@@ -0,0 +1,44 @@
+package Week1_Longest_Substring_without_repeat;
+import java.util.Scanner;
+import org.junit.Assert;
+public class Longest_Substring_Without_Repeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter your String");
+		String str=s.nextLine();
+		System.out.println(lengthOfLongestSubstring(str));
+
+	}
+    public static int lengthOfLongestSubstring(String s) {
+    	if(s==null) {
+    		System.out.println("You have entered a null String");
+    		return -1;
+    	}
+		int start = 0, curr_Len = 0, max_Len = Integer.MIN_VALUE;
+		int characters[] = new int[256];
+		int i;
+		for (i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			int index = (int) ch;
+
+			if (characters[index] != 0 && characters[index] >= start) {
+				curr_Len = (i - start);
+				start = characters[index];
+				max_Len = Math.max(curr_Len, max_Len);
+			
+
+			}
+			characters[index] = i + 1; // +1 is used because if I have used 0 based indexing then it would not check
+										// for 0th index character
+
+		}
+		curr_Len = i - start;
+    	
+		
+		return Math.max(curr_Len, max_Len);
+    	
+	}
+  
+}
